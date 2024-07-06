b624108739e9c19f52a473ff9497902e254cad39
imanav10 <manavrj.07@gmail.com>
2024-07-07 01:11:19 +0530

    yikes

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,4 +1,16 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
-    const jumpAmt = Math.floor(Math.sqrt(Number))
-
+    const jumpAmt = Math.floor(Math.sqrt(breaks.length))
+    let i = jumpAmt;
+    for(; i < breaks.length; i+=jumpAmt){
+        if(breaks[i]){
+            break;
+        }
+    }
+    i-=jumpAmt;
+    for(let j=0; j< jumpAmt && i < breaks.length; ++j, ++i){
+        if(breaks[i]){
+            return i;
+        }
+    }
+    return -1;
 }
