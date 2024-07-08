27544f2ee0b4494f96bee2401f13cd9c2b65cc34
imanav10 <manavrj.07@gmail.com>
2024-07-08 19:41:04 +0530

    hiihih

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,38 @@
 export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+    const seen = new  Array(graph.length).fill(false);
+    const prev = new  Array(graph.length).fill(-1);
+    seen[source] = true;
+    const q: number[] = [source];
+    do{
+        const curr = q.shift() as number;
+        if(curr == needle){
+            break;
+        }
+        const adj = graph[curr];
+        for(let i=0; i< adj.length;++i){
+            if(adj[i]==0){
+                continue;
+            }
+            if(seen[i]){
+                continue;
+            }
+            seen[i]=true;
+            prev[i]=curr;
+            q.push(i)
+        }
+        seen[curr]=true;
+    } while (q.length);
 
+
+    let curr =needle;
+    const Out: number[]= [];
+
+    while (prev[curr] !== -1){
+        Out.push(curr);
+        curr = prev[curr]
+    }
+    if(Out.length){
+        return [source].concat(Out.reverse());
+    }
+    return null;
 }
--- src/day1/LRU.ts
+++ src/day1/LRU.ts
@@ -1,9 +1,26 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+    prev?: Node<T>;
+}
+
+function createNode<V>(value: V){
+    return{value} as Node<V>
+}
+
+
 export default class LRU<K, V> {
     private length: number;
+    private head?: Node<V>;
+    private tail?: Node<V>;
+    private lookup: new Map<K,Node<V>>();
+    private reverselookup: new Map<Node<V>, K>();
 
-    
 
-    constructor() {
+    constructor(private capacity: number = 10) {
+        this.length = 0;
+        this.head = this.tail = undefined; 
+        this.lookup = new Map<K, Node<V>>();
     }
 
     update(key: K, value: V): void {
